baf0cf734edc548889cb2b4a8c9899017df2087a
Saturn Karry <skarry@si-f5.kzn.21-school.ru>
2020-11-15 19:40:35 +0300

    add struct philo

--- philo_one/philo.h
+++ philo_one/philo.h
@@ -6,7 +6,7 @@
 /*   By: skarry <skarry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 17:48:39 by skarry            #+#    #+#             */
-/*   Updated: 2020/11/15 19:23:11 by skarry           ###   ########.fr       */
+/*   Updated: 2020/11/15 19:39:32 by skarry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,6 @@
 # define WHT	"\x1B[37m"
 # define BOLD	"\033[1m"
 
-typedef	struct			s_philo
-{
-	int					id;
-	int					t2die;
-	int					t2eat;
-	int					t2sleep;
-	int					count_eat;
-	int					die;
-}						t_philo;
-
 typedef	struct			s_table
 {
 	int					philo;
@@ -46,6 +36,15 @@ typedef	struct			s_table
 	int					count_eat;
 	int					die;
 }						t_table;
+
+typedef	struct			s_philo
+{
+	int					id;
+	int					birth;
+	int					last_meal;
+	int					die;
+	t_table				table;
+}						t_philo;
 
 int		put_table(t_table *table, int ac, char **av);
 void	ft_putstr(char *s);
